2ee3d82ea196db7dd4b6cd2b4b2a2bb8b117f484
Alec Khoury <akhoury.ak@gmail.com>
2020-11-25 13:58:15 -0600

    Tweak headers for axios request

--- pages/index.vue
+++ pages/index.vue
@@ -121,6 +121,7 @@ export default {
           },
           headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
           },
         }
       )
